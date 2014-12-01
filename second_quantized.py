7931bd067bb79561319a109bc034f91721d7978c
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-12-01 21:43:51 +0000

    A worthwhile optimization

--- second_quantized.py
+++ second_quantized.py
@@ -2,19 +2,9 @@
 from math import *
 ir2 = 1/sqrt(2)
 
-def swap(m1, m2, state):
-    for key, value in state:
-        key.replace(m1, m2)
-        key.replace(m2, m1)
+a=range(20)
 
-def bs(m1, m2, state):
-    pass  
+state=[a[0], a[1], a[3]]
+print state
 
-def pprint(state):
-    for key, value in state:
-        print "|%s> \t %f" % (",".join(map(str,key)), value)
-
-state = [["abf", ir2], ["cdf", ir2]]
-swap(5,6,state)
-#pprint(state)
 
--- test.py
+++ test.py
@@ -2,19 +2,17 @@ from pprint import pprint
 import linear_optics as lo
 import numpy as np
 
-circuit = [{'type': 'bellpair', 'pos': {'x': 0, 'y': 0}},
-  {'type': 'sps', 'pos': {'x': 0, 'y': 5}},
-  {'type': 'phaseshifter', 'pos': {'x': 2, 'y': 0}, 'phase':0},
-  {'type': 'coupler', 'pos': {'x': 2, 'y': 1}, 'ratio': 0.5},
-  {'type': 'coupler', 'pos': {'x': 3, 'y': 1}, 'ratio': 0.5},
-  {'type': 'crossing', 'pos': {'x': 2, 'y': 3}},
-  {'type': 'crossing', 'pos': {'x': 4, 'y': 4}}]
+def test():
+    circuit = [{'type': 'source', 'x': 0, 'y': 0},
+        {'type': 'source', 'x': 0, 'y': 1},
+      {'type': 'coupler', 'x': 1, 'y': 0, 'ratio': 0.5},
+      {'type': 'coupler', 'x': 1, 'y': 10, 'ratio': 0.5}]
 
-circuit = lo.compile(circuit)
-pprint(circuit)
+    circuit = lo.compile(circuit)
+    output_state = lo.simulate(**circuit)
 
-output_state = lo.simulate(**circuit)
+    for key, value in sorted(output_state.items(), key=lambda x:x[0]):
+        if abs(value)>0: print "|%s> : %s" % (key, value)
 
-for key, value in sorted(output_state.items(), key=lambda x:x[0]):
-    if abs(value)>0: print "|%s> : %s" % (key, value)
-
+if __name__=="__main__":
+    test()
