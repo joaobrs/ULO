898ea263c8ef91f55aca58fba561d26f24468a13
Pete Shadbolt <pete.shadbolt@gmail.com>
2016-07-31 19:21:43 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "ulo",
-    version = "0.1.0",
+    version = "0.1.1",
     packages = ["ulo"],
     test_suite = "tests",
     author = "Pete Shadbolt",
