3545ae47cf8ddb6a2d8f723fd3d2649795ed61c9
Pete Shadbolt <pete.shadbolt@gmail.com>
2016-08-01 18:51:07 -0700

    Gonna try another tack

--- tests/test_circuit.py
+++ tests/test_circuit.py
@@ -1,20 +1,30 @@
 import numpy as np
 from ulo import Circuit
 
+def bsu(reflectivity):
+    """ Used for testing """
+    return "bsu({})".format(reflectivity)
+
 def test_circuit():
     c = Circuit(name="Test circuit")
     assert c.name == "Test circuit"
 
 def test_dynamic_beamsplitter():
-    def beamsplitter_unitary(reflectivity):
-        return "ubs({})".format(reflectivity)
-    
-    #Beamsplitter = Circuit(name="Beamsplitter", unitary = np.eye(2))
-    #print Beamsplitter.get_unitary()
-    Beamsplitter = Circuit(name="Beamsplitter", unitary = beamsplitter_unitary)
-    print Beamsplitter(0, reflectivity=.69).get_unitary()
-    #Beamsplitter = Circuit(name="Beamsplitter", unitary = beamsplitter_unitary(.5))
+    """ This is a really wierd way of programming. Who can say if it is good? """
+    Beamsplitter = Circuit(name="Beamsplitter", unitary=bsu)
+    assert Beamsplitter(0, reflectivity="test").get_unitary() == ["bsu(test) @ 0"]
 
-    #Beamsplitter(5, reflectivity=.1)
+class Circuit:
+
+class BeamSplitter(circuit):
+    def __init__(self, reflectivity):
+        self.unitary = 
+
+class Fusion(Circuit):
+    components = [
+            DualBeamsplitter(1, 2, 3, 4), 
+            Swap(2, 3),
+            DualBeamsplitter(1, 2, 3, 4), 
+    ]
 
 
