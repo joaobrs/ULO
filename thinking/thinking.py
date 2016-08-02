dac1f6930e39138e0c16d9b3b62a6df0fa0229bc
Pete Shadbolt <pete.shadbolt@gmail.com>
2016-08-01 17:32:28 -0700

    Still getting somewhere

--- thinking/thinking.py
+++ thinking/thinking.py
@@ -1,33 +1,38 @@
+
 class Circuit(object):
-    def __init__(self, name, *things, **kwargs):
-        self.name = name
+    def __init__(self, *things, **kwargs):
         self.things = things
+        self.name = kwargs.get("name", "Unnamed")
         self.unitary = kwargs.get("unitary", None)
         self.position = kwargs.get("position", None)
 
     def __call__(self, *position, **kwargs):
-        return Circuit(self.name, *self.things, position=position)
+        return Circuit(*self.things, name=self.name, unitary=self.unitary, position=position)
 
     def __str__(self):
-        return "{} ({}), pos={}".format(self.name, ", ".join(map(str, self.things)), self.position)
+        s = "{}: (modes: {})".format(self.name, self.position)
+        for thing in self.things:
+            for line in str(thing).split("\n"):
+                s += "\n.   "+line
+        return s
 
 
     
 if __name__ == '__main__':
     # All of the following should run!
-    BS = Circuit("BS", unitary="U")
-    print BS
-    print BS.unitary
-    Fusion = Circuit("Fusion", BS(0), BS(1))
+    Beamsplitter = Circuit(name="Beamsplitter", unitary="Ubs")
+
+    Swap = Circuit(name="Swap", unitary="Uswap")
+
+    TwoBS = Circuit(Beamsplitter(0, 1), Beamsplitter(2, 3), name="Beamsplitter pair")
+    Fusion = Circuit(TwoBS(0,1,2,3), Swap(1, 2), TwoBS(0, 1, 2, 3), name="Type-II fusion")
+
     print Fusion
     print Fusion()
-    TwoFusions = Circuit("TwoFusions", Fusion(0, 1), Fusion(1, 2))
-    print TwoFusions
-    #print Fusion(1)
+    print Fusion(1)
 
 
-    #print Circuit(HWP(0))
-
+    # All of the following should run!
     # Polarization encoding
     #Fusion = Circuit(HWP(0), HWP(1), PBS(0, 1), HWP(0), HWP(1))
 
